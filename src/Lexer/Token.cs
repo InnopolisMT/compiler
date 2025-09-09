bd62f7d28211837533925156bf70da58ab5010cf
MarkCesium <danikhrenkov29@gmail.com>
2025-09-10 02:02:24 +0300

    Remake Token class as ABC, add additional classes

--- src/Lexer/Token.cs
+++ src/Lexer/Token.cs
@@ -1,17 +1,46 @@
 namespace Compiler.Lexer;
 
-public class Span(int line, int posStart, int posEnd)
+public class Span(int line, int start, int end)
 {
-    public int Line { get; set; } = line;
-    public int PosStart { get; set; } = posStart;
-    public int PosEnd { get; set; } = posEnd;
+    public int Line { get; } = line;
+    public int Start { get; } = start;
+    public int End { get; } = end;
+
+    public override string ToString() => $"{Line}:{Start}-{End}";
 }
 
-public class Token(TokenType type, string value, Span span)
+public abstract class Token(TokenType type, string lexeme, Span span)
 {
     public TokenType Type { get; } = type;
-    public string Value { get; } = value;
     public Span Span { get; } = span;
+    public string Lexeme { get; } = lexeme;
 
-    public override string ToString() => $"{Type}: {Value}";
+    public override string ToString() => $"{Type}: {Lexeme}";
+}
+
+// Токен для идентификаторов
+public class IdentifierToken(string lexeme, Span span) : Token(TokenType.tkIdentifier, lexeme, span)
+{
+    public string Name => Lexeme;
+}
+
+// Токен для целых чисел
+public class IntegerToken(string lexeme, long value, Span span) : Token(TokenType.tkIntegerLiteral, lexeme, span)
+{
+    public long Value { get; } = value;
+}
+
+// Токен для вещественных чисел
+public class RealToken(string lexeme, double value, Span span) : Token(TokenType.tkRealLiteral, lexeme, span)
+{
+    public double Value { get; } = value;
+}
+
+public class BooleanToken(string lexeme, bool value, Span span) : Token(TokenType.tkBoolLiteral, lexeme, span)
+{
+    public bool Value { get; } = value;
+}
+
+public class SimpleToken(TokenType type, string lexeme, Span span) : Token(type, lexeme, span)
+{
 }
