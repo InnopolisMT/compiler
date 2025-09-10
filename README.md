cc54a0f793bd35647db874b67232be3621da52f2
MarkCesium <danikhrenkov29@gmail.com>
2025-09-10 03:21:03 +0300

    Add first test and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # compiler
+
+## To-do's
+- Add new-line token '\n'
+
+## Questions
+
+- Is field in record shoud be represented in token as identifier or field?
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using Compiler.Lexer;
+
+namespace Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string input = @"
+                var person : record
+                    salary : real
+                    age : integer
+                end
+                
+                person.name := 2351.11
+                person.age := 30
+                
+                for i in person.age..50 loop
+                    print i
+                end
+            ";
+
+            var lexer = new LexerClass(input);
+            var tokens = lexer.NextToken();
+
+            while (tokens.Type != TokenType.tkEOF)
+            {
+                Console.WriteLine(tokens);
+                tokens = lexer.NextToken();
+            }
+        }
+    }
+}
